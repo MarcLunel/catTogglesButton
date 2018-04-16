21d0c60dade8cf0507b61d8e468c75b6f91a0889
Marc <marc.l64@outlook.fr>
2018-04-16 23:48:42 +0200

    Hide cat box and protect system from spam of users

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,24 @@
+$widthBox = $('.cat-box-top').width();
+$position = $('.switch-label').offset().left - $('.cat-paw').width() - $('.cat-paw').offset().left + 120 + 10;
+$action = true;
+
 $('.switch-label').click(function(){
-  $position = $('.switch-label').offset().left - $('.cat-paw').width() - $('.cat-paw').offset().left + 120 + 10;
+  if($action == true){
+    $action = false;
     $('.cat-paw').animate({"left":$position+"px"},300);
+    $('.cat-box-top').css({"box-shadow":"15px 0px 10px -2px rgba(0,0,0,0.5)", "width":$widthBox-20+"px"});
     $('.cat-paw').animate({"left":"0px"},300);
-  setTimeout(function(){
-    $('.switch-input').attr("disabled", true);
-  }, 100);
-  setTimeout(function(){
-    $('.switch-input').attr("disabled", false);
-    $('.switch-input').prop('checked', false);
-  }, 270);
-  setTimeout(function(){
-    $('.cat-paw').stop();
-  }, 600);
-}).stop();
+    setTimeout(function(){
+      $('.switch-input').attr("disabled", true);
+    }, 1);
+    setTimeout(function(){
+      $('.switch-input').prop('checked', false);
+    }, 270);
+    setTimeout(function(){
+      $('.switch-input').attr("disabled", false);
+      $('.cat-box-top').css({"box-shadow":"none", "width":$widthBox+"px"});
+      $('.cat-paw').stop();
+      $action = true;
+    }, 600);
+  }
+})
